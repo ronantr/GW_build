3d413b695a42505659ec5df9c23c2fec64a2c7a7
ronan <noob93500@gmail.com>
2024-06-17 08:34:38 +0200

    Fix image and taget blank in link

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,7 +6,7 @@ import Reaper_icon from './icons/Reaper_icon.vue'
 import Renegade_icon from './icons/Renegade_icon.vue'
 import Scourge_icon from './icons/Scourge_icon.vue'
 import Scrapper_icon from './icons/Scrapper_icon.vue'
-import Holosmith_icon from './icons/Vindicator_icon.vue'
+import Holosmith_icon from './icons/Holosmith_icon.vue'
 </script>
 
 <template>
@@ -15,49 +15,49 @@ import Holosmith_icon from './icons/Vindicator_icon.vue'
       <Firebrand_icon />
     </template>
     <template #heading>FB burn</template>
-    <a href="https://metabattle.com/wiki/Build:Firebrand_-_SA_Burn_(GvG)">build</a>
+    <a href="https://metabattle.com/wiki/Build:Firebrand_-_SA_Burn_(GvG)" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Druid_icon />
     </template>
     <template #heading>Druid Support</template>
-    <a href="https://metabattle.com/wiki/Build:Druid_-_Support_Druid_(GvG)">build</a>
+    <a href="https://metabattle.com/wiki/Build:Druid_-_Support_Druid_(GvG)" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Reaper_icon />
     </template>
     <template #heading>Reaper Condi</template>
-    <a href="https://metabattle.com/wiki/Build:Reaper_-_Cele_Shouts_(GvG)">build</a>
+    <a href="https://metabattle.com/wiki/Build:Reaper_-_Cele_Shouts_(GvG)" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Renegade_icon />
     </template>
     <template #heading>Renegade Condi Alac</template>
-    <a href="https://metabattle.com/wiki/Build:Renegade_-_Cele_Renegade_(GvG)">build</a>
+    <a href="https://metabattle.com/wiki/Build:Renegade_-_Cele_Renegade_(GvG)" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Scourge_icon />
     </template>
     <template #heading>Scrouge Cele transfu</template>
-    <a href="https://metabattle.com/wiki/Build:Scourge_-_Condition_Scourge">build</a>
+    <a href="https://metabattle.com/wiki/Build:Scourge_-_Condition_Scourge" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Scrapper_icon />
     </template>
     <template #heading>Scrapper Supp</template>
-    <a href="https://metabattle.com/wiki/Build:Scrapper_-_Med_Kit_Scrapper">build</a>
+    <a href="https://metabattle.com/wiki/Build:Scrapper_-_Med_Kit_Scrapper" target="_blank">build</a>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
       <Holosmith_icon />
     </template>
     <template #heading>Holosmith DPS</template>
-    <a href="https://metabattle.com/wiki/Build:Holosmith_-_DPS_Holo">build</a>
+    <a href="https://metabattle.com/wiki/Build:Holosmith_-_DPS_Holo" target="_blank">build</a>
   </WelcomeItem>
 
 </template>
